ad119c687fb4d22bc8fc83b324069bd0b0c58444
Conner <conner610@gmail.com>
2018-01-07 16:00:44 -0500

    Created Projector Class, and began work on UI

---
+++ NECProjectorController/NECProjectorController/VirtualProjector.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NECProjectorController {
+    // This virtual projector object that will be altered by the controller
+
+    class VirtualProjector {
+
+    }
+}
