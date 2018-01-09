c1134fbdc7455d1147bcc4e599125377b5185ec1
Conner <conner610@gmail.com>
2018-01-09 09:18:30 -0500

    Completed the UI for Power, Volume, and Input. Added power/volume to virtual projector class. Added connection class

--- NECProjectorController/NECProjectorController/MainWindow.xaml.cs
+++ NECProjectorController/NECProjectorController/MainWindow.xaml.cs
@@ -14,12 +14,61 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace NECProjectorController {
-    
+
     // Contoller for the main application window
 
+    // activeInput color: #FF5F7D8B
+    // inactiveInput color: #FF41494D
+
     public partial class MainWindow : Window {
+        // Brush converter for hex values
+        BrushConverter bc = new BrushConverter();
+
+        private VirtualProjector vp = new VirtualProjector();
+
+        // Initialization
         public MainWindow() {
             InitializeComponent();
+
+            // Any label/component initialization that is needed
+            volumeLabel.Content = "Volume: " + vp.GetVolume();
         }
+
+        // The event handler for the power button
+        private void powerButton_Click(object sender, RoutedEventArgs e) {
+            bool ps = vp.GetPowerStatus(); // Temporary holder for the powerStatus
+            ps = !ps; // Alternate power status
+
+            vp.SetPowerStatus(ps); // Change the powerStatus in the virtual object
+
+            // Set the appearance of the power button (Brush)bc.ConvertFrom("#FFXXXXXX")
+            if (ps) {
+                powerButton.Background = (Brush)bc.ConvertFrom("#FF613737");
+                powerButton.Content = "OFF";
+                powerLabel.Content = "Power: On";
+            } else {
+                powerButton.Background = (Brush)bc.ConvertFrom("#FF376150");
+                powerButton.Content = "ON";
+                powerLabel.Content = "Power: Off";
+            }
+        }
+
+        // Handler for any of the input presses
+        private void input_Click(object sender, RoutedEventArgs e) {
+            
+        }
+
+        // Volume Up Handler
+        private void volumeUp_Click(object sender, RoutedEventArgs e) {
+            vp.IncrementVolume(); // Increment volume, then change the label
+            volumeLabel.Content = "Volume: " + vp.GetVolume();
+        }
+
+        // Volume Down Handler
+        private void volumeDown_Click(object sender, RoutedEventArgs e) {
+            vp.DecrementVolume(); // Decrement volume, then change the label
+            volumeLabel.Content = "Volume: " + vp.GetVolume();
+        }
+
     }
 }
--- NECProjectorController/NECProjectorController/VirtualProjector.cs
+++ NECProjectorController/NECProjectorController/VirtualProjector.cs
@@ -9,5 +9,36 @@ namespace NECProjectorController {
 
     class VirtualProjector {
 
+        // Instance Variables
+        private bool powerStatus = false;
+        private int[] input = new int[] { 0, 1, 2, 3, 4, 5 }; // VGA1, VGA2, Video, Component, HDMI1, HDMI2, LAN/Network
+        private int activeInput = 0;
+        private bool isMuted = false;
+        private int volume = 20;
+
+        // Constructor
+        public VirtualProjector() { }
+
+        // Get/Set for powerStatus
+        public bool GetPowerStatus() => powerStatus;
+        public void SetPowerStatus(bool ps) { this.powerStatus = ps; }
+
+        // Get/Set for activeInput
+        public int GetActiveInput() => activeInput;
+        public void SetActiveInput(int activeInput) { this.activeInput = activeInput; }
+
+        // Get & increment/decrement for Volume
+        public int GetVolume() => volume;
+        public void IncrementVolume() { volume += 1; }
+        public void DecrementVolume() { volume -= 1; }
+
+
+        public void PrintState() {
+            Console.WriteLine("Power: " + powerStatus);
+            Console.WriteLine("Active Input: " + activeInput);
+            Console.WriteLine("Volume: " + volume);
+            Console.WriteLine("Is Muted: " + isMuted);
+        }
+
     }
 }
